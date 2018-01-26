fbb4edac5ab6d8c73bb48fb1551d3dfb1106c806
Sajed RasouliFar <26543746+Sajed@users.noreply.github.com>
2018-01-26 00:22:16 -0800

    Rename 2-2.c to 0-9 ca counter.c

--- 0-9 counter - CA -common anode/0-9 ca counter.c
+++ 0-9 counter - CA -common anode/0-9 ca counter.c
@@ -2,7 +2,7 @@
 This program was produced by the
 CodeWizardAVR V2.05.3 Standard
 Automatic Program Generator
-© Copyright 1998-2011 Pavel Haiduc, HP InfoTech s.r.l.
+Â© Copyright 1998-2011 Pavel Haiduc, HP InfoTech s.r.l.
 http://www.hpinfotech.com
 
 Project : 
